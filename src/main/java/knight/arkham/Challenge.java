bbfbe18171e7ca62a9cdfa3837dfc57307a352ab
Knight <karvin_jimenez@hotmail.com>
2022-12-07 21:34:55 -0400

    refactorizando proyecto

--- src/main/java/knight/arkham/Challenge.java
+++ src/main/java/knight/arkham/Challenge.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Challenge {
 
     private static List<String> loadFileData(String filename) {
-        File file = new File(filename);
+        var file = new File(filename);
         Scanner scanner = null;
         try {
             scanner = new Scanner(file);
@@ -18,7 +18,7 @@ public class Challenge {
             e.printStackTrace();
         }
 
-        List<String> fileValues = new ArrayList<>();
+        var fileValues = new ArrayList<String>();
 
         if (scanner != null) {
 
@@ -39,11 +39,18 @@ public class Challenge {
 
         for (String content : backpacksContent) {
 
+            System.out.println(content);
+
+//            getPriorityBadgeSum(content);
             totalSumOfItemsPriority += getPriorityItemSum(content);
         }
 
-        System.out.println(totalSumOfItemsPriority);
+        printChallengeResults(3, totalSumOfItemsPriority, 0);
     }
+
+//    private static void getPriorityBadgeSum(String backpackContent){
+//
+//    }
 
 //    To help prioritize item rearrangement, every item type can be converted to a priority:
 //    Lowercase item types a through z have priorities 1 through 26.
@@ -56,15 +63,15 @@ public class Challenge {
         var firstCompartmentItems = new char[firstCompartment.length()];
         var secondCompartmentItems = new char[secondCompartment.length()];
 
-        for (int i = 0; i < firstCompartment.length(); i++)
+        for (var i = 0; i < firstCompartment.length(); i++)
             firstCompartmentItems[i] = firstCompartment.charAt(i);
 
-        for (int i = 0; i < secondCompartment.length(); i++)
+        for (var i = 0; i < secondCompartment.length(); i++)
             secondCompartmentItems[i] = secondCompartment.charAt(i);
 
-        int itemRepeatCounter = 0;
+        var itemRepeatCounter = 0;
 
-        int sumOfItemsPriority = 0;
+        var sumOfItemsPriority = 0;
 
         var itemsPriority = getItemsPriorityMap();
 
@@ -91,13 +98,13 @@ public class Challenge {
 
         int priorityValue = 1;
 
-        for (char letter = 'a'; letter <= 'z'; ++letter){
+        for (var letter = 'a'; letter <= 'z'; ++letter){
 
             itemsPriority.put(String.valueOf(letter), priorityValue);
             priorityValue++;
         }
 
-        for (char letter = 'A'; letter <= 'Z'; ++letter){
+        for (var letter = 'A'; letter <= 'Z'; ++letter){
 
             itemsPriority.put(String.valueOf(letter), priorityValue);
             priorityValue++;
@@ -109,12 +116,12 @@ public class Challenge {
 
     public static void rockPaperAndScissorsChallenge() {
 
-        List<String> roundValues = loadFileData("tournament.txt");
+        var tournamentRoundValues = loadFileData("tournament.txt");
 
         int totalScorePart1 = 0;
         int totalScorePart2 = 0;
 
-        for (String roundValue : roundValues) {
+        for (var roundValue : tournamentRoundValues) {
 
             totalScorePart1 = getTournamentTotalScore(totalScorePart1, roundValue, true);
             totalScorePart2 = getTournamentTotalScore(totalScorePart2, roundValue, false);
@@ -125,13 +132,13 @@ public class Challenge {
 
     private static int getTournamentTotalScore(int totalScore, String roundValue, boolean isPart1) {
 
-        int rockScore = 1;
-        int paperScore = 2;
-        int scissorsScore = 3;
+        var rockScore = 1;
+        var paperScore = 2;
+        var scissorsScore = 3;
 
-        int lost = 0;
-        int draw = 3;
-        int win = 6;
+        var lost = 0;
+        var draw = 3;
+        var win = 6;
 
         //Rival variables: A = Rock, B = Paper, C = Scissor
         //My Variables Part 1: X = Rock, Y = Paper, Z = Scissor
@@ -171,14 +178,14 @@ public class Challenge {
 
     public static void caloriesChallenge() {
 
-        List<String> calories = loadFileData("calories.txt");
+        var calories = loadFileData("calories.txt");
 
         int actualSum = 0;
         int maxValue = 0;
 
-        List<String> totalCaloriesOfEachElf = new ArrayList<>();
+        var totalCaloriesOfEachElf = new ArrayList<String>();
 
-        for (String actualCalories : calories) {
+        for (var actualCalories : calories) {
 
             if (actualCalories.equals("")) {
 
@@ -193,17 +200,17 @@ public class Challenge {
                 actualSum += Integer.parseInt(actualCalories);
         }
 
-        int arraySize = totalCaloriesOfEachElf.size() - 1;
+        var arraySize = totalCaloriesOfEachElf.size() - 1;
 
-        int totalCaloriesOfTop3 = Integer.parseInt(totalCaloriesOfEachElf.get(arraySize)) +
+        var totalCaloriesOfTop3 = Integer.parseInt(totalCaloriesOfEachElf.get(arraySize)) +
                 Integer.parseInt(totalCaloriesOfEachElf.get(arraySize - 1)) +
                 Integer.parseInt(totalCaloriesOfEachElf.get(arraySize - 2));
 
         printChallengeResults(1, maxValue, totalCaloriesOfTop3);
     }
 
-    private static void printChallengeResults(int challengeNumber, int maxValue, int totalCaloriesOfTop3) {
+    private static void printChallengeResults(int challengeNumber, int challenge1, int challenge2) {
         System.out.println("\nReto #" + challengeNumber + " Completado");
-        System.out.println("Respuesta Parte 1: " + maxValue + "\nRespuesta Parte 2: " + totalCaloriesOfTop3);
+        System.out.println("Respuesta Parte 1: " + challenge1 + "\nRespuesta Parte 2: " + challenge2);
     }
 }
