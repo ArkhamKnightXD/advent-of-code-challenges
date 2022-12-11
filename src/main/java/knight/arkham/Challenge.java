ba1e9ae3a3e6829c6c2cfd5f74b14105ee2be626
Knight <karvin_jimenez@hotmail.com>
2022-12-11 15:33:13 -0400

    Trabajando Reto #5

--- src/main/java/knight/arkham/Challenge.java
+++ src/main/java/knight/arkham/Challenge.java
@@ -18,33 +18,187 @@ public class Challenge {
         return scanner;
     }
 
-    public static void supplyStacksChallenge(){
+    public static void supplyStacksChallenge() {
 
         var cargoInstructions = loadFileData("supply-stacks.txt");
 
-        var stack = new Stack<String>();
+        var stack1 = new Stack<String>();
+        var stack2 = new Stack<String>();
+        var stack3 = new Stack<String>();
+        var stack4 = new Stack<String>();
+        var stack5 = new Stack<String>();
+        var stack6 = new Stack<String>();
+        var stack7 = new Stack<String>();
+        var stack8 = new Stack<String>();
+        var stack9 = new Stack<String>();
 
-        //        Para resolver el problema debo de utilizar push, pop y peek
-//        stack.push("test");
-//        stack.peek()
+        var list1 = new ArrayList<String>();
+        var list2 = new ArrayList<String>();
+        var list3 = new ArrayList<String>();
+        var list4 = new ArrayList<String>();
+        var list5 = new ArrayList<String>();
+        var list6 = new ArrayList<String>();
+        var list7 = new ArrayList<String>();
+        var list8 = new ArrayList<String>();
+        var list9 = new ArrayList<String>();
 
         int lineCounter = 0;
 
-        while (cargoInstructions.hasNextLine() && lineCounter < 5){
-            System.out.println(cargoInstructions.nextLine());
+        while (cargoInstructions.hasNextLine() && lineCounter < 8) {
 
             lineCounter++;
+
+            var instruction = cargoInstructions.nextLine();
+
+//            Hay 4 es char entre cada letra del stack
+            if (!String.valueOf(instruction.charAt(1)).trim().isEmpty())
+                list1.add(String.valueOf(instruction.charAt(1)));
+
+            if (!String.valueOf(instruction.charAt(5)).trim().isEmpty())
+                list2.add(String.valueOf(instruction.charAt(5)));
+
+            if (!String.valueOf(instruction.charAt(9)).trim().isEmpty())
+                list3.add(String.valueOf(instruction.charAt(9)));
+
+            if (!String.valueOf(instruction.charAt(13)).trim().isEmpty())
+                list4.add(String.valueOf(instruction.charAt(13)));
+
+            if (!String.valueOf(instruction.charAt(17)).trim().isEmpty())
+                list5.add(String.valueOf(instruction.charAt(17)));
+
+            if (!String.valueOf(instruction.charAt(21)).trim().isEmpty())
+                list6.add(String.valueOf(instruction.charAt(21)));
+
+            if (!String.valueOf(instruction.charAt(25)).trim().isEmpty())
+                list7.add(String.valueOf(instruction.charAt(25)));
+
+            if (!String.valueOf(instruction.charAt(29)).trim().isEmpty())
+                list8.add(String.valueOf(instruction.charAt(29)));
+
+            var finalCargoElement = String.valueOf(instruction.charAt(instruction.length() - 2));
+
+            if (!finalCargoElement.trim().isEmpty())
+                list9.add(finalCargoElement);
+        }
+
+        fillTheStackFromAnList(stack1, list1);
+        fillTheStackFromAnList(stack2, list2);
+        fillTheStackFromAnList(stack3, list3);
+        fillTheStackFromAnList(stack4, list4);
+        fillTheStackFromAnList(stack5, list5);
+        fillTheStackFromAnList(stack6, list6);
+        fillTheStackFromAnList(stack7, list7);
+        fillTheStackFromAnList(stack8, list8);
+        fillTheStackFromAnList(stack9, list9);
+
+        var cargoStackMap = new HashMap<String, Stack<String>>();
+
+        cargoStackMap.put("1", stack1);
+        cargoStackMap.put("2", stack2);
+        cargoStackMap.put("3", stack3);
+        cargoStackMap.put("4", stack4);
+        cargoStackMap.put("5", stack5);
+        cargoStackMap.put("6", stack6);
+        cargoStackMap.put("7", stack7);
+        cargoStackMap.put("8", stack8);
+        cargoStackMap.put("9", stack9);
+
+        System.out.println("Before: " + cargoStackMap.values());
+
+        while (cargoInstructions.hasNextLine()) {
+
+            lineCounter++;
+
+            var instruction = cargoInstructions.nextLine();
+
+            if (lineCounter > 10){
+
+                System.out.println("Data: " + instruction);
+
+                var instructionLength = instruction.length();
+
+                var destinationKey = String.valueOf(instruction.charAt(instruction.length()-1));
+
+                if (instructionLength == 18){
+
+                    int elementsToMove = Integer.parseInt(String.valueOf(instruction.charAt(5)));
+
+                    var sourceKey = String.valueOf(instruction.charAt(12));
+
+                    var sourceStack = cargoStackMap.get(sourceKey);
+                    var destinationStack = cargoStackMap.get(destinationKey);
+
+                    moveElementsBetweenStacks(elementsToMove, sourceStack, destinationStack);
+                }
+
+                else{
+
+                    int elementsToMove = Integer.parseInt(String.valueOf(instruction.charAt(5)) + instruction.charAt(6));
+
+                    System.out.println("elements To Move: " + elementsToMove);
+
+                    var sourceKey = String.valueOf(instruction.charAt(13));
+
+                    System.out.println("source Key: " + sourceKey);
+                    System.out.println("destination Key: " + destinationKey);
+
+                    var sourceStack = cargoStackMap.get(sourceKey);
+                    var destinationStack = cargoStackMap.get(destinationKey);
+
+                    moveElementsBetweenStacks(elementsToMove, sourceStack, destinationStack);
+                }
+            }
+        }
+
+        System.out.println("after: " + cargoStackMap.values());
+
+
+        for (var actualStack :cargoStackMap.values())
+            System.out.println(actualStack.peek());
+    }
+
+    private static void fillTheStackFromAnList(Stack<String> stack, ArrayList<String> list) {
+        Collections.reverse(list);
+
+        for (var element : list)
+            stack.push(element);
+    }
+
+    private static void moveElementsBetweenStacks(int elementsToMove, Stack<String> source, Stack<String> destination) {
+
+        if (elementsToMove == 1) {
+
+            var elementToMove = source.pop();
+
+            destination.push(elementToMove);
+        } else {
+
+            var auxList = new ArrayList<String>();
+
+            var stackElements = source.iterator();
+
+            while (stackElements.hasNext() && auxList.size() < elementsToMove)
+                auxList.add(stackElements.next());
+
+//        Revierto el orden los elementos de la lista debido a que el orden no es correcto para el stack
+            Collections.reverse(auxList);
+
+            for (var element : auxList) {
+
+                destination.push(element);
+                source.pop();
+            }
         }
     }
 
-    public static void campCleanupChallenge(){
+    public static void campCleanupChallenge() {
 
         var cleaningSections = loadFileData("cleaning-pairs.txt");
 
         int part1Counter = 0;
         int part2Counter = 0;
 
-        while (cleaningSections.hasNextLine()){
+        while (cleaningSections.hasNextLine()) {
 
             var sectionPairs = cleaningSections.nextLine().split(",");
 
@@ -56,9 +210,9 @@ public class Challenge {
             int minValueSection2 = Integer.parseInt(sectionPair2.split("-")[0]);
             int maxValueSection2 = Integer.parseInt(sectionPair2.split("-")[1]);
 
-            boolean isSection1OverlappingSection2 = (minValueSection1<=minValueSection2 && maxValueSection1>=maxValueSection2);
-            boolean isSection2OverlappingSection1 = (minValueSection1>=minValueSection2 && maxValueSection1<=maxValueSection2);
-            boolean isPartialOverlapping = (minValueSection1<=maxValueSection2 && maxValueSection1>=minValueSection2);
+            boolean isSection1OverlappingSection2 = (minValueSection1 <= minValueSection2 && maxValueSection1 >= maxValueSection2);
+            boolean isSection2OverlappingSection1 = (minValueSection1 >= minValueSection2 && maxValueSection1 <= maxValueSection2);
+            boolean isPartialOverlapping = (minValueSection1 <= maxValueSection2 && maxValueSection1 >= minValueSection2);
 
             if (isSection1OverlappingSection2 || isSection2OverlappingSection1)
                 part1Counter++;
@@ -83,13 +237,13 @@ public class Challenge {
 
         var actualElfGroup = new ArrayList<String>();
 
-        while (backpacksContent.hasNextLine()){
+        while (backpacksContent.hasNextLine()) {
 
             var content = backpacksContent.nextLine();
 
             actualElfGroup.add(content);
 
-            if (actualElfGroup.size() == 3){
+            if (actualElfGroup.size() == 3) {
 
                 totalSumOfBadgePriority += getPriorityBadgeSum(actualElfGroup, itemsPriority);
 
@@ -104,7 +258,7 @@ public class Challenge {
         printChallengeResults(3, totalSumOfItemsPriority, totalSumOfBadgePriority);
     }
 
-    private static int getPriorityBadgeSum(List<String> elfGroup, HashMap<String, Integer> itemsPriority){
+    private static int getPriorityBadgeSum(List<String> elfGroup, HashMap<String, Integer> itemsPriority) {
 
         var backpackGroupMember1 = elfGroup.get(0);
         var backpackGroupMember2 = elfGroup.get(1);
@@ -117,13 +271,13 @@ public class Challenge {
         int itemRepeatCounter = 0;
         int sumOfBadgePriority = 0;
 
-        for (char content1 :backpackItems1) {
+        for (char content1 : backpackItems1) {
 
-            for (char content2 :backpackItems2) {
+            for (char content2 : backpackItems2) {
 
-                for (char content3 :backpackItems3) {
+                for (char content3 : backpackItems3) {
 
-                    if ((content1 == content2 && content2 == content3) && itemRepeatCounter == 0){
+                    if ((content1 == content2 && content2 == content3) && itemRepeatCounter == 0) {
 
                         var itemPriorityValue = itemsPriority.get(String.valueOf(content1));
 
@@ -184,13 +338,13 @@ public class Challenge {
 
         int priorityValue = 1;
 
-        for (var letter = 'a'; letter <= 'z'; letter++){
+        for (var letter = 'a'; letter <= 'z'; letter++) {
 
             itemsPriorityMap.put(String.valueOf(letter), priorityValue);
             priorityValue++;
         }
 
-        for (var letter = 'A'; letter <= 'Z'; letter++){
+        for (var letter = 'A'; letter <= 'Z'; letter++) {
 
             itemsPriorityMap.put(String.valueOf(letter), priorityValue);
             priorityValue++;
@@ -207,7 +361,7 @@ public class Challenge {
         int totalScorePart1 = 0;
         int totalScorePart2 = 0;
 
-        while (tournamentRoundValues.hasNextLine()){
+        while (tournamentRoundValues.hasNextLine()) {
 
             var roundValue = tournamentRoundValues.nextLine();
 
@@ -275,7 +429,7 @@ public class Challenge {
 
         var totalCaloriesOfEachElf = new ArrayList<String>();
 
-        while (calories.hasNextLine()){
+        while (calories.hasNextLine()) {
 
             var actualCalories = calories.nextLine();
 
