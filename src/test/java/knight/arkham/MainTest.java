f4b7a003fb03fcc1c0242a5462992c62384d04b2
karvin <karvin.jimenez@alternard.com>
2023-01-06 16:47:26 -0400

    Agregado Test Junit

---
+++ src/test/java/knight/arkham/MainTest.java
@@ -0,0 +1,12 @@
+package knight.arkham;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testSum() {
+        Assert.assertEquals(17, 17);
+    }
+}
